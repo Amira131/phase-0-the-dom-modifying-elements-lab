e3f8f4e59a6cf5fd809a586a667c5b806435032a
Amira de Moya <demoyaamira@gmail.com>
2022-05-07 18:36:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+main.remove("main")
+
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory";
+
+newHeader.textContent = "Amira is the champion"
+document.body.append(newHeader)
+
